9397474ccc762c83739e60bf5f3d0852492996cc
vincent <vincent@Vincent>
2025-01-16 10:09:42 +0800

    init database

---
+++ migrations/20250115082531_initial.sql
@@ -0,0 +1,40 @@
+-- Add migration script here
+-- create user table sql in postgres
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    fullname VARCHAR(64) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    -- hashed argon2 password
+    password VARCHAR(255) NOT NULL,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP
+);
+
+-- add unique index for email
+CREATE UNIQUE INDEX IF NOT EXISTS email_idx ON users (email);
+
+-- create chat type: single, group, private_channel, public_channel
+CREATE TYPE chat_type AS ENUM ('single', 'group', 'private_channel', 'public_channel');
+
+-- create chat table
+CREATE TABLE IF NOT EXISTS chats (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    type chat_type NOT NULL,
+    --user id list
+    members BIGINT[] NOT NULL,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP
+);
+
+-- create message table
+CREATE TABLE IF NOT EXISTS messages (
+    id BIGSERIAL PRIMARY KEY,
+    chat_id BIGINT NOT NULL REFERENCES chats(id),
+    sender_id BIGINT NOT NULL REFERENCES users(id),
+    content TEXT NOT NULL,
+    images TEXT[],
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP
+);
+
+-- add index for chat_id
+CREATE INDEX IF NOT EXISTS messages_chat_id_idx ON messages (chat_id, created_at DESC);
+CREATE INDEX IF NOT EXISTS messages_sender_id_idx ON messages (sender_id, created_at DESC);
